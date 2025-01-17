adaa11d91f81d50fb3419bf955c9ef9d76f0b5cf
SahilMaral <sahilmaral14@gmail.com>
2025-01-17 16:34:50 +0530

    new files

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const jsonfile = require('jsonfile');
 const moment = require('moment');
 const simpleGit = require('simple-git');
+const random = require('random');
 
-const FILE_PATH = './data.json';
-
-const DATE = moment().format();
-
-const data = {
-    date: DATE
+function getRandomInRange(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
 }
 
-jsonfile.writeFile(FILE_PATH, data);
 
-simpleGit().add([FILE_PATH]).commit(DATE, { '--date' : DATE}).push();
+const FILE_PATH = './data.json';
+const makeCommit = n => {
+    if (n === 0) return simpleGit().push();
+    const weeks = getRandomInRange(0, 54);
+    const days = getRandomInRange(0, 6);
+    const DATE = moment().subtract(2, 'y').add(1, 'd').add(weeks, 'w').add(days, 'd').format();
+    const data = { date: DATE };
+    console.log(DATE);
+    jsonfile.writeFile(FILE_PATH, data, () => {
+        simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }, makeCommit.bind(this, --n));
+    });
+}
+// weeks - for weeks goes form 0 - 54
+// days - for days goes form 0 - 6
+makeCommit(1000);
